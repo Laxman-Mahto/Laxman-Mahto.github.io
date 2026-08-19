34666bd3ab0b5ba42bd62067f8ce823317ea298d
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-19 23:00:41 +0530

    Resume Updated

--- src/Components/Sections/About.tsx
+++ src/Components/Sections/About.tsx
@@ -105,7 +105,7 @@ const About: React.FC = () => {
 
         {/* Resume Download Button */}
         <a
-          href="/Laxman(Resume)3.pdf"
+          href="/Laxman_Mahto_Resume.pdf"
           download="Laxman_Mahto_Resume.pdf"
           rel="noopener noreferrer"
           className="inline-flex items-center justify-center gap-2 sm:gap-3 rounded-xl 
